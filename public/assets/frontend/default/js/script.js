267cfd35d21d87ec07368b9a1f83cb4bff2f0ca5
khaled312001 <khaledahmed.201188@gmail.com>
2026-03-25 19:00:35 +0200

    fix(js): patch slider error on missing jQuery UI plugin

--- public/assets/frontend/default/js/script.js
+++ public/assets/frontend/default/js/script.js
@@ -53,23 +53,25 @@ $(document).ready(function () {
 
 
     // Pricing Range
-    $("#slider-range").slider({
-        range: true,
-        orientation: "horizontal",
-        min: 0,
-        max: 10000,
-        values: [0, 10000],
-        step: 100,
+    if ($.fn.slider) {
+        $("#slider-range").slider({
+            range: true,
+            orientation: "horizontal",
+            min: 0,
+            max: 10000,
+            values: [0, 10000],
+            step: 100,
 
-        slide: function (event, ui) {
-            if (ui.values[0] == ui.values[1]) {
-                return false;
+            slide: function (event, ui) {
+                if (ui.values[0] == ui.values[1]) {
+                    return false;
+                }
+
+                $("#min_price").val(ui.values[0]);
+                $("#max_price").val(ui.values[1]);
             }
-
-            $("#min_price").val(ui.values[0]);
-            $("#max_price").val(ui.values[1]);
-        }
-    });
+        });
+    }
 
 
     $('.gSearch-icon').on('click', function () {
